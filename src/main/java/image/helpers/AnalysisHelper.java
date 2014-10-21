f14843c2b3d903913a04ed3590a623d962236c73
Marios Kotsiandris <marioskots@gmail.com>
2014-10-22 00:15:13 +0300

    Implement more calculations

--- src/main/java/image/helpers/AnalysisHelper.java
+++ src/main/java/image/helpers/AnalysisHelper.java
@@ -4,18 +4,25 @@ package image.helpers;
  * Created by cerebro on 10/19/14.
  */
 public class AnalysisHelper {
+
 	final double PI = 3.14;
 
-	double circularity, length, width, feretDiameter;
+	double circularity = 0.0;
+	double length = 0.0;
+	double width = 0.0;
+	double feretDiameter = 0.0;
+	double volume = 0.0;
 
-	public AnalysisHelper(double circularity, double length, double width, double feretDiameter){
+	public AnalysisHelper(double circularity, double length, double width, double feretDiameter)
+	{
 		this.circularity = circularity;
 		this.length = length;
 		this.width = width;
 		this.feretDiameter = feretDiameter;
 	}
 
-	public  double findApproximateVolume(double circularity){
+	public  double findApproximateVolume(double circularity)
+	{
 		if (circularity > 0){
 			return this.getEqualSphericalVolume(this.feretDiameter, this.length, this.width);
 		} else {
@@ -23,15 +30,32 @@ public class AnalysisHelper {
 		}
 	}
 
-	private double getEqualCylinderVolume(double feretDiameter, double length, double width) {
+	private double getEqualCylinderVolume(double feretDiameter, double length, double width)
+	{
 		double radius = feretDiameter/2;
 		double volume = PI*radius*radius*length;
 		return volume;
 	}
 
-	private double getEqualSphericalVolume(double feretDiameter, double length, double width) {
+	private double getEqualSphericalVolume(double feretDiameter, double length, double width)
+	{
 		double radius = feretDiameter/2;
 		double volume = PI*4*radius*radius*radius/3;
 		return volume;
+	}
+
+	public double getSurfaceDiameter(double area)
+	{
+		return Math.sqrt(area/PI);
+	}
+
+	public double getVolumeDiameter(double volume)
+	{
+		return Math.cbrt(6*volume/PI);
+	}
+
+	public double getVolumeToSurface(double volume, double area)
+	{
+		return 6*volume / area;
 	}
 }
